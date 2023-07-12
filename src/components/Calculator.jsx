d0d6d69c28e20ee05e88b53bb81d40603550f98d
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-07-12 11:53:10 -0600

    Add calculator function

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
+import calculate from '../logic/calculate.js';
 import './Calculator.css';
 
 function Calculator() {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
   const buttons = ['AC', '+/-', '%', '÷', 7, 8, 9, 'x', 4, 5, 6, '-', 1, 2, 3, '+', 0, '.', '='];
+
+  const handleClick = (buttonName) => {
+    const result = calculate({ total, next, operation }, buttonName);
+    setTotal(result.total);
+    setNext(result.next);
+    setOperation(result.operation);
+  };
+
+  const display = (total || '') + (operation || '') + (next || '');
 
   return (
     <div className="calculator">
-      <div className="result">0</div>
+      <div className="result">{display || '0'}</div>
       {buttons.map((buttonValue) => (
-        <Button key={buttonValue} value={buttonValue} />
+        <Button key={buttonValue} value={buttonValue} handleClick={handleClick} />
       ))}
     </div>
   );
