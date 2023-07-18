ef249be062a7a36706470d040a3255ad520b1a82
Henok Mekonnen <henokmekonnen5@gmail.com>
2023-07-18 12:39:03 -0600

    add calculator test

--- src/__tests__/Calculator.test.js
+++ src/__tests__/Calculator.test.js
@@ -1,16 +1,32 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+// Calculator.test.js
+import { render, fireEvent, screen } from '@testing-library/react';
 import Calculator from '../components/Calculator';
 
 describe('Calculator', () => {
-  it('renders Calculator correctly', () => {
+  it('renders the calculator', () => {
     const { asFragment } = render(<Calculator />);
     expect(asFragment()).toMatchSnapshot();
   });
 
   it('updates display when a number button is clicked', () => {
     render(<Calculator />);
-    fireEvent.click(screen.getByText('1'));
+    fireEvent.click(screen.getByRole('button', { name: /1/i }));
     expect(screen.getByText('1')).toBeInTheDocument();
+  });
+
+  it('performs an operation when an operation button is clicked', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByRole('button', { name: /1/i }));
+    fireEvent.click(screen.getByRole('button', { name: /\+/i }));
+    fireEvent.click(screen.getByRole('button', { name: /2/i }));
+    fireEvent.click(screen.getByRole('button', { name: /=/i }));
+    expect(screen.getByText('3')).toBeInTheDocument();
+  });
+
+  it('clears the display when the AC button is clicked', () => {
+    render(<Calculator />);
+    fireEvent.click(screen.getByRole('button', { name: /1/i }));
+    fireEvent.click(screen.getByRole('button', { name: /AC/i }));
+    expect(screen.getByText('0')).toBeInTheDocument();
   });
 });
