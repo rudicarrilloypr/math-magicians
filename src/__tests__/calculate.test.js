cd844e4504618107d546ebee008d579f49517d31
Rodolfo Carrillo <rudicarrilloypr@gmail.com>
2023-07-27 14:27:38 -0600

    Center home text

--- src/__tests__/calculate.test.js
+++ src/__tests__/calculate.test.js
@@ -5,7 +5,7 @@ describe('calculate function', () => {
   it('updates the number on the display when a number button is clicked', () => {
     let data = { total: null, next: null, operation: null };
     data = calculate(data, '5');
-    expect(data).toEqual({ total: null, next: '5', operation: undefined }); // Cambiado null a undefined
+    expect(data).toEqual({ total: null, next: '5', operation: undefined });
   });
 
   it('updates the operation when an operation button is clicked', () => {
